238ffcea5fed175dcb1ee5a74f530849c031f3bd
Nikhil Razz <nikhilhash03@gmail.com>
2026-05-21 10:38:52 +0530

    Add project files.

---
+++ thiskeyword/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thiskeyword
+{
+    class Nikhil
+    {
+        int a;
+        int b;
+        public Nikhil(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public void add()
+        {
+            int c = a + b;
+            Console.WriteLine("sum is "+c);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nikhil obj = new Nikhil(10, 30);
+            obj.add();
+        }
+    }
+}
